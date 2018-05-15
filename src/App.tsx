e03d95131eec8d0054c8368f877526b0029d800c
rksquared <rahul@rahulkrish.com>
2018-05-15 17:12:06 -0400

    added wiki iframe, hardcoded graph in d3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { Layout } from "antd";
 import * as React from 'react';
 import './App.css';
-
+import StaticWiki from './components/staticWiki';
+import Force from './dataViz/Force';
+import { IData } from './globalTypes';
 import logo from './logo.png';
 
-class App extends React.Component {
+
+const { Content } = Layout;
+
+interface IAppState {
+  data: IData;
+  input: string;
+  search: string;
+}
+
+export default class App extends React.Component<{}, IAppState> {
+
+  public state = {
+    data: {
+      "links": [
+        { "source": "Napoleon", "target": "Myriel", "value": 1 },
+        { "source": "Mlle.Baptistine", "target": "Myriel", "value": 8 },
+      ],
+      "nodes": [
+        { "id": "Myriel", "group": 1 },
+        { "id": "Napoleon", "group": 1 },
+        {"id": "Mlle.Baptistine", "group": 1}
+      ],
+
+    },
+    input: 'Julius_Caesar',
+    search: 'Julis_Caesar',
+  }
+
+  public divElement: any = {};
+
   public render() {
     return (
       <div className="App">
@@ -11,12 +43,36 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        <div className="App-intro">
+        <input placeholder={"input a search!"} value={this.state.input} onChange={this.handleInput}/>
+        <button onClick={this.searchDaWikis}>Submit!</button>
+        </div>
+        <StaticWiki q={this.state.search}/>
+        <Content
+          style={{ margin: '2rem', padding: '2rem', background: '#fff' }}
+        >
+          <div ref={divElement => { this.divElement = divElement }} >
+            <Force width={window.innerWidth * .9} height={window.innerHeight / 2} data={this.state.data} condRender={this.condRender} />
+          </div>
+        </Content>
+        </div>
     );
   }
-}
 
-export default App;
+  private condRender = () => {
+
+    return true;
+  }
+
+  private handleInput = (e: any) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  private searchDaWikis = () => {
+    this.setState({
+      search: this.state.input
+    });
+  }
+}
---
+++ src/dataViz/Force.tsx
@@ -0,0 +1,168 @@
+/* tslint:disable:no-console jsx-no-lambda */
+import * as d3 from 'd3';
+import * as React from 'react';
+import { Component } from 'react';
+import { IData } from '../globalTypes';
+
+interface IProps {
+  width: number;
+  height: number;
+  data: IData;
+  condRender: () => void;
+}
+
+interface IRefs {
+  mountPoint?: HTMLDivElement | null;
+}
+
+export default class ForceGraph extends Component<IProps> {
+  
+  private ctrls: IRefs = {};
+  private force: any;
+  
+  public componentDidMount() {
+    this.renderForce();
+  }
+  
+  public componentDidUpdate() {
+    console.log(' shouldupdating component')
+      this.removeForce();
+      this.renderForce();
+  }
+  
+  public shouldComponentUpdate(nextProps: IProps, nextState: any) {
+  
+    return JSON.stringify(this.props) === JSON.stringify(nextProps) ?
+    (JSON.stringify(this.state) === JSON.stringify(nextState) ? false : true) :
+    true;
+  }
+  
+  public render() {
+    const { width, height } = this.props;
+    const style = {
+      backgroundColor: '#333',
+      height,
+      width
+    };
+  return (
+    <div>
+      <div style={style} ref={mountPoint => (this.ctrls.mountPoint = mountPoint)} />
+    </div>)
+  }
+
+  
+  private removeForce() {
+    const force: any = this.ctrls.mountPoint;
+    
+    while(force.hasChildNodes()) {
+      force.removeChild(force.lastChild);
+    }
+  }
+  
+  private renderForce() {
+    const { width, height, data } = this.props;
+    console.log('rendering de Force', data);
+    const radius: number = data.nodes.length > 500 ? 40 * 300/data.nodes.length : height > 1800 ? 44 : 30;
+    
+    if (this.ctrls.mountPoint !== undefined) {
+
+      this.force = d3
+        .forceSimulation()
+        .nodes(data.nodes)
+        .force(
+          'charge', 
+          d3
+            .forceManyBody()
+            .strength(
+              data.nodes.length > 500 ? -200 : 
+              data.nodes.length > 100 ? -1400 : 
+              height > 1800 ? (data.nodes.length > 60 ? -3000 : data.nodes.length > 30 ? -4000 : -10000) :
+              -3200))
+        .force("link", d3.forceLink(data.links).id((d: any) => d.id))
+        .force('center', d3.forceCenter(width / 2, height / 2.2));
+
+      const svg = d3
+        .select(this.ctrls.mountPoint)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+      const link = svg
+        .append("g")
+        .attr("class", "links")
+        .selectAll('line')
+        .data(data.links)
+        .enter()
+        .append('line')
+        .style('stroke', '#999999')
+        .style('stroke-opacity', 0.6)
+        .style('stroke-width', d => 8);
+        // .style('stroke-width', d => Math.sqrt(d.value) * 2)
+
+
+      const color = d3.scaleOrdinal(d3.schemeCategory10);
+      const node = svg
+        .selectAll('circle')
+        .data(data.nodes)
+        .enter()
+        .append<SVGCircleElement>('circle')
+        .attr('r', radius)
+        .style('stroke', '#FFFFFF')
+        .style('stroke-width', 2)
+        .style('fill', (d: any) => color(d.group))
+        .call(
+          d3
+            .drag()
+            .on('start', ((d: any) => this.dragStarted(d, this.force)))
+            .on('drag', this.dragged)
+            .on('end', ((d: any) => this.dragEnded(d, this.force))),
+      )
+
+      const labels = svg.selectAll(".mytext")
+            .data(data.nodes)
+            .enter()
+            .append("text")
+              .text((d: any) => d.id )
+              .style("text-anchor", "middle")
+              .style("fill", "white")
+              .style("font-family", "Arial")
+              .style("font-size", 12)
+              .on("click", this.props.condRender)
+
+      this.force.on('tick', () => {
+        link
+          .attr('x1', (d: any) => d.source.x)
+          .attr('y1', (d: any) => d.source.y)
+          .attr('x2', (d: any) => d.target.x)
+          .attr('y2', (d: any) => d.target.y);
+
+        // node.attr('cx', (d: any) => d.x).attr('cy', (d: any) => d.y);
+        node.attr("cx", (d: any) => d.x = Math.max(radius, Math.min(width - radius, d.x)))
+        .attr("cy", (d: any) => d.y = Math.max(radius, Math.min(height - radius, d.y)));
+
+        labels
+          .attr("x", (d: any) => d.x )
+          .attr("y", (d: any) => d.y);
+      });
+    }
+  }
+  private dragStarted(d: any, force: any) {
+    if (!d3.event.active) {
+      force.alphaTarget(0.3).restart();
+    }
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  private dragged(d: any) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  private dragEnded(d: any, force: any) {
+    if (!d3.event.active) { force.alphaTarget(0)};
+    // d.fx = null;
+    // d.fy = null;
+  }
+
+}
